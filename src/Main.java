73ed86337c131bacd784cbfa680215966014d4f5
unknown <jakubwrunczas@gmail.com>
2025-09-23 12:09:17 +0200

    aktualizacja

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,68 @@
+import java.util.Scanner;
+import java.math.MathContext;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //System.out.println("Wproawdz stopnie celsjusza:");
+        Scanner text = new Scanner(System.in);
+/*
+        float celsius = text.nextFloat();
+        float fahrenheit = (float)1.8 * celsius + (float)32.0;
+        System.out.println(celsius+ " Stopni celsjusza to:\n" + fahrenheit +  " Stopni Fahreneita");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Podaj 3 liczby:");
+        int a , b, c;
+        a = text.nextInt();
+        b = text.nextInt();
+        c = text.nextInt();
+        int[] tablica = {a,b,c};
+        int max = a;
+        int min = a;
+        for (int i = 0; i < tablica.length; i++) {
+            int currentnum = tablica[i];
+            if(currentnum > max)
+                max = currentnum;
+            if(currentnum < min)
+                min = currentnum;
         }
+        System.out.println("Najmniejsza: "+ min);
+        System.out.println("Najwieksza: " + max);
+
+        System.out.println("Podaj wagę kg:");
+        float waga = text.nextFloat();
+        System.out.println("Podaj wzrost cm:");
+        float wzrost = text.nextFloat();
+        wzrost = wzrost /100;1
+        float bmi = (waga) / (wzrost * wzrost);
+        System.out.println("BMI: " + bmi);
+
+        if(bmi > 24.9) {
+            System.out.println("Nadwaga");
+        }
+        else if (bmi < 18.5) {
+            System.out.println("Niedowaga");
+        }else {
+            System.out.println("waga prawidłowa");
+        }
+
+        System.out.println("Podaj dohod w tym roku:");
+        float podatek;
+        float dochod = text.nextFloat();
+        float podatek1 = (float) 85.528;
+        if(dochod <=  85.528) {
+            podatek = ( (dochod  /100 ) * 18)  - (float)556.02;
+
+        } else {
+            float nadwyzka = dochod - podatek1;
+            podatek = (float)14839.02 + ((nadwyzka / 100) * 32);
+        }
+        System.out.println("Podatek to: " + podatek);
+*/
+        return;
     }
 }
